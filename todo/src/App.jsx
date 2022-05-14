b66e68739ccfd4ae3bfa2a70459a68645b7f3515
Romulo Costa Segato <romulocs38@gmail.com>
2022-05-14 09:13:25 -0300

    progresso 1

---
+++ todo/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar Programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <div>
+      <div className="container">
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default App;
