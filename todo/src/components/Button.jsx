c0cd769b20bd0a8008779fff6feda89f04606b20
Romulo Costa Segato <romulocs38@gmail.com>
2022-05-16 17:29:58 -0300

    progresso4

--- todo/src/components/Button.jsx
+++ todo/src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Button.css";
 
-const Button = ({ children }) => {
-  return <button onClick={} className="button">{children}</button>;
+const Button = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
