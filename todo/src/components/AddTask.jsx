162e554d05f85ee7d7ab333998eb91a97f74ad96
Romulo Costa Segato <romulocs38@gmail.com>
2022-05-17 12:28:26 -0300

    progresso5

--- todo/src/components/AddTask.jsx
+++ todo/src/components/AddTask.jsx
@@ -11,16 +11,18 @@ const AddTask = ({ handleTaskAddition }) => {
 
   const handleAddTaskClick = () => {
     handleTaskAddition(inputData);
+    setInputData("");
   };
 
   return (
     <div className="add-task-container">
       <input
         onChange={handleInputChange}
+        value={inputData}
         className="add-task-input"
         type="text"
       />
-      ;
+
       <div className="add-task-button-container">
         <Button onClick={handleAddTaskClick}>Adicionar</Button>
       </div>
---
+++ todo/src/components/TaskDetails.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Button from "./Button";
+
+const TaskDetails = () => {
+  return (
+    <>
+      <div className="back-button-container">
+        <Button>Voltar</Button>
+      </div>
+      <div className="task-details container">
+        <p></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In maxime
+          sint eos omnis deleniti expedita!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default TaskDetails;
